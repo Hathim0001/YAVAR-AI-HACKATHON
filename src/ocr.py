a5e6ad2991b62e4d614f99a94ace89289d3a0317
Hathim0001 <mohammedhathim0001@gmail.com>
2025-05-29 21:43:32 +0530

    small changes

--- src/ocr.py
+++ src/ocr.py
@@ -11,4 +11,4 @@ def extract_text_with_confidence(image):
     data = pytesseract.image_to_data(image, output_type=pytesseract.Output.DICT, config=custom_config)
     confidences = [float(conf) for conf in data["conf"] if int(conf) > 0]
     avg_confidence = sum(confidences) / len(confidences) if confidences else 0
-    return text, avg_confidence
+    return text, avg_confidence     
